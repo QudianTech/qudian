6a20b84ae538668e68ea0ac3a6d2e8dacba37764
qudianso <qudianso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-23 03:10:12 +0000

    add tag 0.1.1
    git-svn-id: https://plugins.svn.wordpress.org/qudian/tags/0.1.1@717074 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inline.js
@@ -0,0 +1,10 @@
+(function(){
+if (!window._pintex) {
+  window._pintex = {
+    auth: [ "1362799309", "44fc9ea58e1697c85506d305a20dbfea" ],
+  };
+  new pintex.qfo.Qlet(function () {
+    new pintex.qfo.Qfo();
+  });
+}
+}());
