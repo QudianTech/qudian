36be26c0215f76dcacf047ae0fdd9bd67f7e980b
qudianso <qudianso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-21 10:48:51 +0000

    update qfo to 0.3.0
    git-svn-id: https://plugins.svn.wordpress.org/qudian/tags/0.3.0@862257 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inline.js
@@ -0,0 +1,10 @@
+(function () {
+  if (!window._pintex) {
+    window._pintex = {
+      auth: [ "1362799311", "f4c15b80568d050297541e36c7beabe1" ],
+    };
+    new pintex.qfo.Qlet(function () {
+      new pintex.qfo.Qfo();
+    });
+  }
+}());
