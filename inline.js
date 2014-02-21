923827c7a193a02bce11886038eb904d20c20c87
qudianso <qudianso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-21 11:08:32 +0000

    update qfo to 0.3.0
    git-svn-id: https://plugins.svn.wordpress.org/qudian/trunk@862267 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inline.js
+++ inline.js
@@ -1,10 +1,8 @@
-(function () {
+(function() {
   if (!window._pintex) {
-    window._pintex = {
-      auth: [ "1362799311", "f4c15b80568d050297541e36c7beabe1" ],
+    window._pintex={
+      auth:['1362799311','f4c15b80568d050297541e36c7beabe1'],auto:1
     };
-    new pintex.qfo.Qlet(function () {
-      new pintex.qfo.Qfo();
-    });
+    document.body.appendChild(document.createElement('script')).src='http://api.qudian.so/static/js/qlet-v0.3.min.js';
   }
-}());
+})();
